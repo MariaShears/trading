5c7ad219ce2c8c6f0966860c77007d9cc72df079
MariaEgrv <egorova.maria301@gmail.com>
2020-04-19 00:07:56 +0200

    change to SQLite

--- cli_traders_journal.py
+++ cli_traders_journal.py
@@ -1,43 +1,27 @@
-import psycopg2
-import config
 import click
+import sqlite3
 
-
-
-import@click.command()
+@click.command()
 @click.option('--instrument', prompt='Instrument',
               help='The instrument used in trade')
 def insert_in_db(instrument):
-    try:
-        # connect to the PostgreSQL database
-        conn = psycopg2.connect(
-            database="trading_journal",
-            user="masha",
-            host="127.0.0.1",
-            password="trading",
-            port="5432"
-        )
-        # create a new cursor
-        cur = conn.cursor()
-        sql = """ INSERT INTO journal(instrument) VALUES(%s)"""
-        variables = (instrument)
-        # execute the INSERT statement
-        cur.execute(sql, variables)
-        # commit the changes to the database
-        conn.commit()
-        count = cur.rowcount
-        print(count, "Record inserted successfully into journal")
-    except (Exception, psycopg2.Error) as error:
-        if (conn):
-            print("Failed to insert record into journal", error)
-    finally:
-            # closing database connection.
-        if (conn):
-            cur.close()
-            conn.close()
-            print("PostgreSQL connection is closed")
 
-    return instrument
+    try:
+        sqliteConnection = sqlite3.connect('traders_journal.db')
+        cursor = sqliteConnection.cursor()
+        variables = (instrument,)
+        sql = '''INSERT INTO journal(instrument) VALUES(?)'''
+        cursor.execute(sql, variables)
+        sqliteConnection.commit()
+        count = cursor.rowcount
+        print(count, "Record inserted successfully into journal")
+    except sqlite3.Error as error:
+        print("Failed to insert record into journal", error)
+    finally:
+        if (sqliteConnection):
+            cursor.close()
+            sqliteConnection.close()
+            print("The SQLite connection is closed")
 
 
 if __name__ == '__main__':
